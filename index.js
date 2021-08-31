f0e363710e4da929572b9baf5421c8cc70acfcd6
ShubhamSaini1585 <86958663+ShubhamSaini1585@users.noreply.github.com>
2021-08-31 13:58:06 +0530

    Updating Again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Guys!\n'3.15.220.137
+  const msg = 'Hello Guys!\n'
   res.end(msg);
 });
 
